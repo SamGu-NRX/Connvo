ce434e0daa3d2c1181007ec14ff1b76c33ccecaf
AndrewWang <andrewwang123118@gmail.com>
2025-11-02 17:52:45 -0600

    fixed

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { authkitMiddleware } from "@workos-inc/authkit-nextjs";
 
-export default authkitMiddleware({
+export const middleware = authkitMiddleware({
   publicRoutes: [
     "/",
     "/auth/:path*",
